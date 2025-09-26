8aa2b084a8a1a24f1b99e5a33515679e06e4a8e8
jefin basheer <jefin.s786@gmail.com>
2025-09-26 20:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 58.7 MB (21.07%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let newArray=[]
+    for(let i=1;i<=n;i++)
+    {
+        if(i%3===0&&i%5===0)
+        {
+            newArray.push("FizzBuzz")
+        }
+        else if(i%3===0)
+        {
+            newArray.push("Fizz")
+        }
+        else if(i%5===0)
+        {
+            newArray.push("Buzz")
+        }
+        else{
+           newArray.push(i.toString())
+        }
+       
+    }
+
+    return newArray 
+};
+
+console.log(JSON.stringify(fizzBuzz(5)))
