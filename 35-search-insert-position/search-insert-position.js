d25b03208ffb45cca94deb6afb38fcc19b37a645
jefin basheer <jefin.s786@gmail.com>
2025-09-27 11:25:00 +0530

    Time: 1 ms (14.19%) | Memory: 54.2 MB (43.09%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+
+    for(let i=0;i<=nums.length-1;i++)
+    {
+        if(nums[i]==target||nums[i]>=target)
+          {
+            return i
+          }
+          
+    }
+    return nums.length
+};
+
